d3cddfadd7b3c10666494010f46b7e63fcb537e3
adamzahrannnn <68937792+adamzahrannnn@users.noreply.github.com>
2021-09-22 07:43:50 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// alert ("Selamat Datang")
+// var nama = true;
+
+// while ( nama ) {
+//     var nama = prompt (" masukan nama anda = ");
+//     alert ("halo" + nama);
+
+//     nama = confirm("Try Again?");
+// }
+
+// alert( " oke terimakasih ")
+var nama = true;
+var nama = prompt(" MASUKKAN NAMA ANDA =  ");
+alert ('Selamat Datang '+ nama );
+nama = confirm ("APAKAH KAMU " + nama + "?");
+
+
+if ( nama == true ) {
+       alert (" alhamdulillah berarti programnya lancar ");
+}
+else{ alert  (" baik silahkan hubungi kami ")}
